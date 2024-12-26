e0f8a05afac4c5a28efb00c5bd932ce9fe5291f7
derrick921213 <dlin12457@gmail.com>
2024-12-26 19:29:45 +0800

    Feat: Add password process

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use aes::Aes256;
+use block_padding::{Padding, Pkcs7};
+use cipher::{generic_array::GenericArray, BlockDecrypt, BlockEncrypt, KeyInit};
+use generic_array::typenum::U16;
+use hex::{decode, encode};
 use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -25,4 +30,27 @@ impl Config {
             tools,
         }
     }
+}
+#[allow(dead_code)]
+fn encrypt_password(password: &str, key: &[u8]) -> String {
+    let cipher = Aes256::new(GenericArray::from_slice(key));
+    let mut block: GenericArray<u8, U16> = GenericArray::clone_from_slice(&[0u8; 16]);
+    let pos = password.len();
+    block[..pos].copy_from_slice(password.as_bytes());
+    Pkcs7::pad(&mut block, pos);
+    let mut encrypted_block = block;
+    cipher.encrypt_block(&mut encrypted_block);
+
+    encode(encrypted_block)
+}
+#[allow(dead_code)]
+fn decrypt_password(encrypted_password: &str, key: &[u8]) -> String {
+    let cipher = Aes256::new(GenericArray::from_slice(key));
+    let encrypted_bytes = decode(encrypted_password).expect("Decoding failed");
+
+    let mut block: GenericArray<u8, U16> = GenericArray::clone_from_slice(&encrypted_bytes);
+    cipher.decrypt_block(&mut block);
+
+    let decrypted_bytes = Pkcs7::unpad(&block).unwrap();
+    String::from_utf8(decrypted_bytes.to_vec()).expect("Invalid UTF-8")
 }
